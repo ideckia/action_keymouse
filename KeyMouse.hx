2197119ac490d74a18fa2b0e19b58a60fefa67b0
Josu Igoa <josu.igoa@indaba.lks.es>
2021-11-10 16:00:56 +0100

    add empty string to keyToTap.possibleValues

--- KeyMouse.hx
+++ KeyMouse.hx
@@ -4,7 +4,7 @@ using api.IdeckiaApi;
 
 typedef Props = {
 	@:editable("Key to be tapped.", null, [
-		'backspace', 'delete', 'enter', 'tab', 'escape', 'up', 'down', 'right', 'left', 'home', 'end', 'pageup', 'pagedown', 'f1', 'f2', 'f3', 'f4', 'f5',
+		'', 'backspace', 'delete', 'enter', 'tab', 'escape', 'up', 'down', 'right', 'left', 'home', 'end', 'pageup', 'pagedown', 'f1', 'f2', 'f3', 'f4', 'f5',
 		'f6', 'f7', 'f8', 'f9', 'f10', 'f11', 'f12', 'command', 'alt', 'control', 'shift', 'right_shift', 'space', 'printscreen', 'insert', 'audio_mute',
 		'audio_vol_down', 'audio_vol_up', 'audio_play', 'audio_stop', 'audio_pause', 'audio_prev', 'audio_next', 'audio_rewind', 'audio_forward',
 		'audio_repeat', 'audio_random', 'numpad_0', 'numpad_1', 'numpad_2', 'numpad_3', 'numpad_4', 'numpad_5', 'numpad_6', 'numpad_7', 'numpad_8',
@@ -31,7 +31,7 @@ typedef Props = {
 @:name('keymouse')
 class KeyMouse extends IdeckiaAction {
 	public function execute(currentState:ItemState):js.lib.Promise<ItemState> {
-		if (props.keyToTap != null)
+		if (props.keyToTap != null && props.keyToTap != '')
 			RobotJs.keyTap(props.keyToTap.toLowerCase(), props.modifiers.split(',').map(m -> StringTools.trim(m)));
 
 		if (props.typeString != null)
